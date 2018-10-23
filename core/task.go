c5efc7b96f0d051bacf6e771db2f855080600ae4
lightjames <lightjameslyy@gmail.com>
2018-10-23 11:01:17 +0800

    stargo: change Task.Command type, test Task, passed

--- core/task.go
+++ core/task.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"reflect"
 	"runtime"
+	"strings"
 )
 
+type Func func([]interface{}) interface{}
+
 type Task struct {
-	Command func([]interface{})
+	Command Func
 	Args    []interface{}
 }
 
-func (t *Task) Run() {
-	fmt.Println(runtime.FuncForPC(reflect.ValueOf(t.Command).Pointer()).Name())
-	t.Command(t.Args)
+func (t *Task) Run() interface{} {
+	return t.Command(t.Args)
+}
+
+func (t *Task) TaskInfo() string {
+	var res strings.Builder
+	res.WriteString(runtime.FuncForPC(reflect.ValueOf(t.Command).Pointer()).Name() + "( ")
+	for _, arg := range t.Args {
+		res.WriteString(fmt.Sprintf("%T: %v, ", arg, arg))
+	}
+	res.WriteString(")\n")
+	return res.String()
 }
---
+++ core/task_test.go
@@ -0,0 +1,54 @@
+package core
+
+import (
+	"fmt"
+	"testing"
+)
+
+func sum(args []interface{}) interface{} {
+	res := 0
+	for _, n := range args {
+		res += n.(int)
+	}
+	return res
+}
+
+var testIntPtr = new(int)
+
+func addToIntPtr(args []interface{}) interface{} {
+	intPtr := args[0].(*int)
+	*intPtr = 0
+	nums := args[1].([]int)
+	for _, n := range nums {
+		*intPtr += n
+	}
+	return *intPtr
+}
+
+// 表格驱动测试
+var tests = []struct {
+	task Task
+}{
+	// 匿名函数，参数为空
+	{Task{
+		func(i []interface{}) interface{} {
+			fmt.Println("anonymous function")
+			return nil
+		},
+		nil}},
+	// 同类参数
+	{Task{
+		sum,
+		[]interface{}{1, 2, 3, 4, 5}}},
+	// 不同类参数
+	{Task{
+		addToIntPtr,
+		[]interface{}{testIntPtr, []int{1, 2, 3, 4, 5}}}},
+}
+
+func TestTask_Run(t *testing.T) {
+	for _, tt := range tests {
+		fmt.Println(tt.task.Run())
+		fmt.Println(tt.task.TaskInfo())
+	}
+}
